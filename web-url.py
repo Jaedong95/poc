ea4ae36bacea894e60b8f9e36bb9bb00233e38ed
Finger-cori <badzoo@naver.com>
2025-01-09 10:58:53 +0900

    Update web-url.py

--- web-url.py
+++ web-url.py
@@ -1,29 +1,51 @@
 from flask import Flask, send_file, request, jsonify, Response
-from src import SpeakerDiarizer, AudioFileProcessor, WhisperSTT
+from src import SpeakerDiarizer, AudioFileProcessor, TimeProcessor, WhisperSTT
 from src import DBConnection, TableEditor, PostgresDB
 from src import LLMOpenAI
 from dotenv import load_dotenv
 from datetime import datetime
-from io import BytesIO
-import threading
 import requests
 import base64
 import json
+import io
 import os
 
 
 app = Flask(__name__)
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API')
+with open(os.path.join('./config', 'db_config.json')) as f:
+    db_config = json.load(f)
+
+db_conn = DBConnection(db_config)
+db_conn.connect()
+postgres = PostgresDB(db_conn)
+table_editor = TableEditor(db_conn)
+
+diar_module = SpeakerDiarizer()
+audio_p = AudioFileProcessor()
+time_p = TimeProcessor()
+
+stt_module = WhisperSTT(openai_api_key)
+stt_module.set_client()
+stt_module.load_word_dictionary('/ibk/config/word_dict.json')
+'''with open(os.path.join('./config', 'llm_config.json')) as f:
+    llm_config = json.load(f)
+openai_client = LLMOpenAI(llm_config, openai_api_key)
+openai_client.set_generation_config()
+openai_client.set_grammer_guideline()'''
+
 def custom_json_serializer(obj):
     if isinstance(obj, datetime):
         return obj.isoformat()
     raise TypeError(f"Type {type(obj)} not serializable") 
 
-def map_speaker_with_nested_check(meeting_id, stt_result, diar_seg):
+def map_speaker_with_nested_check(meeting_id, stt_result, diar_segments):
     """ stt_result: conv_id, start_time, end_time, text, conf_id """
     conv_id = stt_result[0]
     print(f'stt_segment: {stt_result}')
-    stt_start, stt_end = stt_result[1], stt_result[2]
-    stt_duration = stt_end - stt_start
+    stt_start, stt_end = stt_result[2], stt_result[3]
     
     candidates = []
     for diar_seg in diar_segments:   # STT 결과값과 겹치는 Diar 구간 탐색 
@@ -33,7 +55,6 @@ def map_speaker_with_nested_check(meeting_id, stt_result, diar_seg):
     
     if not candidates:
         print("No overlapping segments found.")
-        speaker_info = 'Unknown'
         return    # DB 업데이트 x 
 
     for candidate in candidates:
@@ -44,7 +65,7 @@ def map_speaker_with_nested_check(meeting_id, stt_result, diar_seg):
         ]
         if nested_segments:   # 또 다른 발화가 있을 때 
             for nested in nested_segments:
-                if is_similar(nested, stt_segment):
+                if time_p.is_similar(nested, stt_result):
                     updated_result = (conv_id, nested['speaker'])
                     table_editor.edit_poc_conf_log_tb('update', 'ibk_poc_conf_log', data=meeting_id, val=updated_result)
                     return 
@@ -86,14 +107,16 @@ def webhook():
         }
     }
     '''
+    print(f'2: {participant_cnt}')
     data = request.json
     diar_result = data['output']['diarization']
     filtered_result = diar_module.rename_speaker(diar_result, participant_cnt)
     with open(os.path.join(file_path, audio_file_name.split('.')[0] + '.json'), 'w', encoding='utf-8') as f:
         json.dump(filtered_result, f, ensure_ascii=False, indent=4)
+    print(meeting_id)
     stt_results = postgres.get_conf_log_data(meeting_id)
     for stt_result in stt_results: 
-        map_speaker_with_nested_check(stt_result, filtered_result)
+        map_speaker_with_nested_check(meeting_id, stt_result, filtered_result)
     return jsonify({"status": "received"}), 200
 
 
@@ -104,41 +127,23 @@ def run_stt_code():
     global audio_p
     global db_conn
     global table_editor
+    global db_config
 
-    load_dotenv()
-    openai_api_key = os.getenv('OPENAI_API')
-    
     data = request.get_json()
-    meeting_id = data['meeting_id']
-    file_idx = data['section_id']
+    meeting_id = data['meeting_Id']
+    file_idx = data['section_Id']
     encoded_audio = data['audio_file']
     decoded_audio = base64.b64decode(encoded_audio)
     audio_file = io.BytesIO(decoded_audio)
 
-    with open(os.path.join('./config', 'db_config.json')) as f:
-        db_config = json.load(f)
-
-    db_conn = DBConnection(db_config)
-    db_conn.connect()
-    table_editor = TableEditor(db_conn)
-
-    audio_p = AudioFileProcessor()
-    stt_module = WhisperSTT(openai_api_key)
-    stt_module.set_client()
-    stt_module.load_word_dictionary('/ibk/config/word_dict.json')
-    '''with open(os.path.join('./config', 'llm_config.json')) as f:
-        llm_config = json.load(f)
-    openai_client = LLMOpenAI(llm_config, openai_api_key)
-    openai_client.set_generation_config()
-    openai_client.set_grammer_guideline()'''
-    stt_module.transcribe_text(audio_p, audio_file, meeting_id, table_editor)
+    stt_module.transcribe_text(audio_p, audio_file, meeting_id, file_idx, table_editor)
+    return jsonify({"status": "received"}), 200
 
 
 @app.post('/run')
 def run_python_code():
-    global participant_cnt 
-    global meeting_id 
-    global file_idx
+    global participant_cnt
+    global meeting_id
     global file_path    # /ibk/meeting_records
     global diar_module
     global new_file_name     # /ibk/meeting_records/ibk-poc-meeting1.wav
@@ -147,16 +152,19 @@ def run_python_code():
     global db_stt_result_path
     global postgres
 
-    load_dotenv()
+    #load_dotenv()
     data = request.get_json()
     pyannot_api_key = os.getenv('PA_API')
     
     file_path = '/ibk/meeting_records/'
     file_name = data['file_name']     # /ibk/meeting_records/ibk-poc-meeting1.wav
+    print(f'file_path: {file_path}')
+    print(f'file_name: {file_name}')
     participant_cnt = data['participant']
     new_file_name = file_path + file_name.split('/')[-1]
     audio_file_name = new_file_name.split('/')[-1]
-    
+    print(f'audio_file_name: {audio_file_name}')
+    meeting_id = int(audio_file_name.split('_')[1])
     pyannotate_url = "https://api.pyannote.ai/v1/diarize"
     file_url = f"https://ibkpoc.fingerservice.co.kr/stt/?audio_file_name={audio_file_name}"
     webhook_url = "https://ibkpoc.fingerservice.co.kr/stt/result"
@@ -167,14 +175,11 @@ def run_python_code():
         "webhook": webhook_url,
         "url": file_url
     }
-    db_conn = DBConnection(db_config)
-    db_conn.connect()
-    postgres = PostgresDB(db_conn)
+    print(f'1: global_cnt: {participant_cnt}')
 
     stt_result_path = '/ibk/meeting_records/stt_result/' + audio_file_name.replace('.wav', '.json')
     db_stt_result_path = '/home/jsh0630/meeting_records/stt_result/' + audio_file_name.replace('.wav', '.json')
 
-    diar_module = SpeakerDiarizer()
     external_response = requests.post(pyannotate_url, headers=headers, json=data)
     flask_response = Response(
         response=external_response.content,   # 응답 본문
